d5ca789c5ae56da96a7bdf7ea84894704504b119
Terry <terrykuo501@gmail.com>
2021-06-01 17:09:04 +0800

    Initial Commit

---
+++ Muse/MuseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuseApp.swift
+//  Muse
+//
+//  Created by Terry Kuo on 2021/6/1.
+//
+
+import SwiftUI
+
+@main
+struct MuseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
